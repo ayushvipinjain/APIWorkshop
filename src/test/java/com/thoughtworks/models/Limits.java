31afb7570cefcf7ef49f4c4216dd3ed24cbef4ff
Ayush Jain <jayush@thoughtworks.com>
2020-02-28 15:03:10 +0530

    Added Schema Validation test for api responses

---
+++ src/test/java/com/thoughtworks/models/Limits.java
@@ -0,0 +1,17 @@
+
+package com.thoughtworks.models;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.annotation.Generated;
+
+@EqualsAndHashCode
+@Getter
+@Setter
+@ToString
+public class Limits {
+
+}
